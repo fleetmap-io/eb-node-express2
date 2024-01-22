527500a11cea0ff114542dfc06e64f55ba70bde4
jcardus <asklocation.net@gmail.com>
2024-01-22 01:29:09 +0000

    package name

--- app.js
+++ app.js
@@ -2,13 +2,9 @@ const cluster = require('cluster')
 
 if (cluster.isMaster) {
   const cpuCount = require('os').cpus().length
-
-  for (let i = 0; i < cpuCount; i += 1) {
-    cluster.fork()
-  }
+  for (let i = 0; i < cpuCount; i += 1) { cluster.fork() }
 
   cluster.on('exit', function (worker) {
-    // Replace the terminated workers
     console.error(`worker ${worker.id} died :(`)
     cluster.fork()
   })
