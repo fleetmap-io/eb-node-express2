a8343d18048e05489f1624423e7b9970972c1d4d
jcardus <asklocation.net@gmail.com>
2024-02-25 18:47:21 +0000

    log fixtTime

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ if (cluster.isMaster) {
         // sync with the other traccar
         await sendToTraccar(device, position)
       } else {
-        console.log('ignoring', position.attributes.source, device.name)
+        console.log('ignoring', position.fixTime, position.attributes.source, device.name)
       }
       res.end()
     } catch (e) {
